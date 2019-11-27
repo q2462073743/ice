fd3fcb356bd08e50655b08ed47062a361285e36c
q2462073743 <54887133+q2462073743@users.noreply.github.com>
2019-11-27 14:35:32 +0800

    Add files via upload

---
+++ v-for.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        foodList:[
+            {name:'肉包',price:3,discount:0.9},
+            {name:'三鲜包',price:5,discount:0.8},
+            {name:'菜包',price:10,discount:0.1},
+        ]
+    }
+});
